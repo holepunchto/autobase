f7bac392ea977c1be9aa4319b2fef4599952bd4c
Andrew Osheroff <andrewosh@gmail.com>
2022-02-17 11:07:59 +0100

    Extract remote linearization tests into separate file

--- test/local-linearizing.js
+++ test/local-linearizing.js
@@ -5,7 +5,7 @@ const ram = require('random-access-memory')
 const { bufferize, linearizedValues } = require('./helpers')
 const Autobase = require('../')
 
-test('linearizing - three independent forks', async t => {
+test('local linearizing - three independent forks', async t => {
   const output = new Hypercore(ram)
   const writerA = new Hypercore(ram)
   const writerB = new Hypercore(ram)
@@ -52,7 +52,7 @@ test('linearizing - three independent forks', async t => {
   t.end()
 })
 
-test('linearizing - causal writes preserve clock', async t => {
+test('local linearizing - causal writes preserve clock', async t => {
   const output = new Hypercore(ram)
   const writerA = new Hypercore(ram)
   const writerB = new Hypercore(ram)
@@ -91,7 +91,7 @@ test('linearizing - causal writes preserve clock', async t => {
   t.end()
 })
 
-test('linearizing - does not over-truncate', async t => {
+test('local linearizing - does not over-truncate', async t => {
   const output = new Hypercore(ram)
   const writerA = new Hypercore(ram)
   const writerB = new Hypercore(ram)
@@ -149,7 +149,7 @@ test('linearizing - does not over-truncate', async t => {
   t.end()
 })
 
-test('linearizing - can cut out a writer', async t => {
+test('local linearizing - can cut out a writer', async t => {
   const output = new Hypercore(ram)
   const writerA = new Hypercore(ram)
   const writerB = new Hypercore(ram)
@@ -194,7 +194,7 @@ test('linearizing - can cut out a writer', async t => {
   t.end()
 })
 
-test('linearizing - can cut out a writer from the back', async t => {
+test('local linearizing - can cut out a writer from the back', async t => {
   const output = new Hypercore(ram)
   const writerA = new Hypercore(ram)
   const writerB = new Hypercore(ram)
@@ -235,7 +235,7 @@ test('linearizing - can cut out a writer from the back', async t => {
   t.end()
 })
 
-test('linearizing - can cut out a writer from the front', async t => {
+test('local linearizing - can cut out a writer from the front', async t => {
   const output = new Hypercore(ram)
   const writerA = new Hypercore(ram)
   const writerB = new Hypercore(ram)
@@ -276,7 +276,7 @@ test('linearizing - can cut out a writer from the front', async t => {
   t.end()
 })
 
-test('linearizing - can cut out a writer, causal writes', async t => {
+test('local linearizing - can cut out a writer, causal writes', async t => {
   const output = new Hypercore(ram)
   const writerA = new Hypercore(ram)
   const writerB = new Hypercore(ram)
@@ -321,7 +321,7 @@ test('linearizing - can cut out a writer, causal writes', async t => {
   t.end()
 })
 
-test('linearizing - can cut out a writer, causal writes interleaved', async t => {
+test('local linearizing - can cut out a writer, causal writes interleaved', async t => {
   const output = new Hypercore(ram)
   const writerA = new Hypercore(ram)
   const writerB = new Hypercore(ram)
@@ -361,7 +361,7 @@ test('linearizing - can cut out a writer, causal writes interleaved', async t =>
   t.end()
 })
 
-test('linearizing - many writers, no causal writes', async t => {
+test('local linearizing - many writers, no causal writes', async t => {
   const NUM_WRITERS = 10
   const NUM_APPENDS = 11
 
@@ -407,7 +407,7 @@ test('linearizing - many writers, no causal writes', async t => {
   t.end()
 })
 
-test('linearizing - double-linearizing is a no-op', async t => {
+test('local linearizing - double-linearizing is a no-op', async t => {
   const output = new Hypercore(ram)
   const writerA = new Hypercore(ram)
   const writerB = new Hypercore(ram)
@@ -449,107 +449,7 @@ test('linearizing - double-linearizing is a no-op', async t => {
   t.end()
 })
 
-test('linearizing - selects longest remote output', async t => {
-  const writerA = new Hypercore(ram)
-  const writerB = new Hypercore(ram)
-  const writerC = new Hypercore(ram)
-
-  const output1 = new Hypercore(ram)
-  const output2 = new Hypercore(ram)
-  const output3 = new Hypercore(ram)
-
-  const inputs = [writerA, writerB, writerC]
-  const base1 = new Autobase({
-    inputs,
-    localOutput: output1
-  })
-  const base2 = new Autobase({
-    inputs,
-    localOutput: output2
-  })
-  const base3 = new Autobase({
-    inputs,
-    localOutput: output3
-  })
-  base1.start()
-  base2.start()
-  base3.start()
-
-  // Create three independent forks and linearize them into separate outputs
-  for (let i = 0; i < 3; i++) {
-    await base1.append(`a${i}`, [], writerA)
-  }
-
-  await base1.view.update()
-
-  for (let i = 0; i < 2; i++) {
-    await base1.append(`b${i}`, [], writerB)
-  }
-
-  await base2.view.update()
-
-  for (let i = 0; i < 1; i++) {
-    await base1.append(`c${i}`, [], writerC)
-  }
-
-  await base3.view.update()
-
-  {
-    // Should not have to modify output3
-    const base = new Autobase({
-      inputs,
-      outputs: [output3],
-      autostart: true
-    })
-    await base.view.update()
-    t.same(base.view.status.appended, 0)
-    t.same(base.view.status.truncated, 0)
-    t.same(base.view.length, 6)
-  }
-
-  {
-    // Should not have to add B and C
-    const base = new Autobase({
-      inputs,
-      outputs: [output1],
-      autostart: true
-    })
-    await base.view.update()
-    t.same(base.view.status.appended, 3)
-    t.same(base.view.status.truncated, 0)
-    t.same(base.view.length, 6)
-  }
-
-  {
-    // Should select output2
-    const base = new Autobase({
-      inputs,
-      outputs: [output1, output2],
-      autostart: true
-    })
-    await base.view.update()
-    t.same(base.view.status.appended, 1)
-    t.same(base.view.status.truncated, 0)
-    t.same(base.view.length, 6)
-  }
-
-  {
-    // Should select output3
-    const base = new Autobase({
-      inputs,
-      outputs: [output1, output2, output3],
-      autostart: true
-    })
-    await base.view.update()
-    t.same(base.view.status.appended, 0)
-    t.same(base.view.status.truncated, 0)
-    t.same(base.view.length, 6)
-  }
-
-  t.end()
-})
-
-test('linearizing - can dynamically add/remove default outputs', async t => {
+test('local linearizing - can dynamically add/remove default outputs', async t => {
   const writerA = new Hypercore(ram)
   const writerB = new Hypercore(ram)
   const writerC = new Hypercore(ram)
@@ -623,129 +523,7 @@ test('linearizing - can dynamically add/remove default outputs', async t => {
   t.end()
 })
 
-test('linearizing - can locally extend an out-of-date remote output', async t => {
-  const writerA = new Hypercore(ram)
-  const writerB = new Hypercore(ram)
-  const writerC = new Hypercore(ram)
-
-  const output1 = new Hypercore(ram)
-
-  const inputs = [writerA, writerB, writerC]
-  const writerBase = new Autobase({
-    inputs,
-    localOutput: output1,
-    autostart: true
-  })
-  const readerBase = new Autobase({
-    inputs,
-    outputs: [output1],
-    autostart: true
-  })
-
-  for (let i = 0; i < 3; i++) {
-    await writerBase.append(`a${i}`, [], writerA)
-  }
-  await writerBase.view.update()
-  await readerBase.view.update()
-
-  t.same(writerBase.view.status.appended, 3)
-  t.same(writerBase.view.status.truncated, 0)
-  t.same(writerBase.view.length, 3)
-  t.same(readerBase.view.status.appended, 0)
-  t.same(readerBase.view.status.truncated, 0)
-  t.same(readerBase.view.length, 3)
-
-  for (let i = 0; i < 2; i++) {
-    await writerBase.append(`b${i}`, [], writerB)
-  }
-
-  await readerBase.view.update()
-  t.same(readerBase.view.status.appended, 2)
-  t.same(readerBase.view.status.truncated, 0)
-  t.same(readerBase.view.length, 5)
-
-  for (let i = 0; i < 1; i++) {
-    await writerBase.append(`c${i}`, [], writerC)
-  }
-
-  await readerBase.view.update()
-  t.same(readerBase.view.status.appended, 1)
-  t.same(readerBase.view.status.truncated, 0)
-  t.same(readerBase.view.length, 6)
-
-  // Extend C and lock the previous forks (will not reorg)
-  for (let i = 1; i < 4; i++) {
-    await writerBase.append(`c${i}`, writerC)
-  }
-
-  await readerBase.view.update()
-  t.same(readerBase.view.status.appended, 3)
-  t.same(readerBase.view.status.truncated, 0)
-  t.same(readerBase.view.length, 9)
-
-  // Create a new B fork at the back (full reorg)
-  for (let i = 1; i < 11; i++) {
-    await writerBase.append(`b${i}`, [], writerB)
-  }
-
-  await readerBase.view.update()
-  t.same(readerBase.view.status.appended, 19)
-  t.same(readerBase.view.status.truncated, 9)
-  t.same(readerBase.view.length, 19)
-
-  t.end()
-})
-
-test('linearizing - will discard local in-memory view if remote is updated', async t => {
-  const writerA = new Hypercore(ram)
-  const writerB = new Hypercore(ram)
-  const writerC = new Hypercore(ram)
-
-  const output1 = new Hypercore(ram)
-
-  const inputs = [writerA, writerB, writerC]
-  const writerBase = new Autobase({
-    inputs,
-    localOutput: output1,
-    autostart: true
-  })
-  const readerBase = new Autobase({
-    inputs,
-    outputs: [output1],
-    autostart: true
-  })
-
-  for (let i = 0; i < 3; i++) {
-    await writerBase.append(`a${i}`, [], writerA)
-  }
-
-  await writerBase.view.update() // Pull the first 3 nodes into output1
-  await readerBase.view.update()
-  t.same(readerBase.view.status.nodes.length, 0) // It should start up-to-date
-
-  for (let i = 0; i < 2; i++) {
-    await writerBase.append(`b${i}`, [], writerB)
-  }
-
-  await readerBase.view.update() // view extends output1 in memory
-  t.same(readerBase.view.status.nodes.length, 2)
-
-  for (let i = 0; i < 1; i++) {
-    await writerBase.append(`c${i}`, [], writerC)
-  }
-
-  await readerBase.view.update()
-  t.same(readerBase.view.status.nodes.length, 3)
-
-  // Pull the latest changes into the output1
-  await writerBase.view.update()
-  await readerBase.view.update()
-  t.same(readerBase.view.status.nodes.length, 0)
-
-  t.end()
-})
-
-test('can dynamically add a default output', async t => {
+test('local linearizing - can dynamically add a default output', async t => {
   const output = new Hypercore(ram)
   const writerA = new Hypercore(ram)
   const writerB = new Hypercore(ram)
