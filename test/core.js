13dacd2381779adae2da7f2d549b03f8e7ef5eec
HDegroote <75906619+HDegroote@users.noreply.github.com>
2023-04-06 11:39:58 +0200

    No new sessions if closed (hypercore compat) (#41)

--- test/core.js
+++ test/core.js
@@ -18,3 +18,28 @@ test('core - isAutobase property', async t => {
   t.is(linearizedCore.isAutobase, true)
   t.is(!normalCore.isAutobase, true)
 })
+
+test('core -  no new session if closed (hypercore compat)', async t => {
+  const [base] = await create(1, apply, store => store.get('test'))
+  const normalCore = new Hypercore(ram)
+  const linearizedSessionCore = base.view
+  const snapshotSession = linearizedSessionCore.snapshot()
+
+  await Promise.all([
+    linearizedSessionCore.close(),
+    normalCore.close(),
+    snapshotSession.close()
+  ])
+
+  // LinearisedCore.session handles snapshots differently than hypercore.session,
+  // so just testing that explicitly too
+
+  t.exception(() => normalCore.session(), /SESSION_CLOSED/)
+  t.exception(() => normalCore.snapshot(), /SESSION_CLOSED/)
+
+  t.exception(() => linearizedSessionCore.session(), /SESSION_CLOSED/)
+  t.exception(() => linearizedSessionCore.snapshot(), /SESSION_CLOSED/)
+
+  t.exception(() => snapshotSession.session(), /SESSION_CLOSED/)
+  t.exception(() => snapshotSession.snapshot(), /SESSION_CLOSED/)
+})
