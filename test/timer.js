dd81fcbc069665704cbbb329faaa4ba65e24bfa0
Mathias Buus <mathiasbuus@gmail.com>
2023-09-20 11:48:36 +0200

    improve acks (#177)
    * improve acks
    
    * threshold ack uses timer with asap mode if avail
    
    * rename tick threshold
    
    * add more merge tests and bau comment
    
    ---------
    
    Co-authored-by: Christophe Diederichs <chm-diederichs@protonmail.com>

--- test/timer.js
+++ test/timer.js
@@ -51,60 +51,10 @@ test('timer - bump max timeout', t => {
     clearInterval(int)
     t.fail()
     timer.stop()
-  }, 400)
+  }, 600) // [200-400ms] + 200ms test slack since timers
 
   function handler () {
     clearInterval(int)
-    clearTimeout(fail)
-
-    t.pass()
-    timer.stop()
-  }
-})
-
-test('timer - extend', t => {
-  t.plan(2)
-
-  const timer = new Timer(handler, 100)
-
-  timer.bump()
-  timer.extend()
-
-  t.is(timer._interval, 200)
-
-  const pass = setTimeout(() => {
-    t.pass()
-    timer.stop()
-  }, 150)
-
-  function handler () {
-    clearTimeout(pass)
-
-    t.fail()
-    timer.stop()
-  }
-})
-
-test('timer - reset', t => {
-  t.plan(2)
-
-  const timer = new Timer(handler, 50)
-
-  timer.bump()
-  timer.extend()
-  timer.extend()
-  timer.extend()
-
-  t.is(timer._interval, 400)
-
-  timer.reset()
-
-  const fail = setTimeout(() => {
-    t.fail()
-    timer.stop()
-  }, 200)
-
-  function handler () {
     clearTimeout(fail)
 
     t.pass()
