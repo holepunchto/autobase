de480ef483c6a17cf70a675ada394d50233913af
Christophe Diederichs <45171645+chm-diederichs@users.noreply.github.com>
2024-01-31 01:58:23 +0000

    Each view signature is only valid for a specific indexer set (#255)
    * hash indexer set into signer namespace
    
    * hash core prologue into namespace
    
    * only need to hash prologue into namespace
    
    * review by @mafintosh
    
    * remove whitespace
    
    * more review
    
    * system checkout method and sameIndexers helper

--- index.js
+++ index.js
@@ -551,7 +551,8 @@ module.exports = class Autobase extends ReadyResource {
     const w = this.activeWriters.get(key)
     if (!w) return null
 
-    const namespace = w.deriveNamespace(core.name)
+    const { prologue } = core.core.manifest
+    const namespace = w.deriveNamespace(core.name, prologue)
     const publicKey = w.core.manifest.signers[0].publicKey
 
     return {
@@ -1517,14 +1518,23 @@ module.exports = class Autobase extends ReadyResource {
     }
   }
 
-  _generateCheckpoint (cores) {
+  async _generateCheckpoint (cores) {
+    if (!this._addCheckpoints) return null
+
+    // indexer set to sign for
+    const length = this.system.core._source.pendingIndexedLength
+    const checkout = await this.system.checkout(length)
+
+    const migrated = checkout.sameIndexers(this.linearizer.indexers)
+    await checkout.close()
+
     if (this._firstCheckpoint) {
       this._firstCheckpoint = false
       // TODO: unsafe, use an array instead for views as the order is important
-      return generateCheckpoint(this._viewStore.opened.values())
+      return generateCheckpoint(this._viewStore.opened.values(), migrated)
     }
 
-    return generateCheckpoint(cores)
+    return generateCheckpoint(cores, migrated)
   }
 
   async _flushLocal (localNodes) {
@@ -1533,13 +1543,24 @@ module.exports = class Autobase extends ReadyResource {
     const cores = this._addCheckpoints ? this._viewStore.getIndexedCores() : []
     const blocks = new Array(localNodes.length)
 
+    let migrated = false
+    let closing = null
+
+    if (this._addCheckpoints) {
+      const length = this.system.core._source.pendingIndexedLength
+      const checkout = await this.system.checkout(length)
+
+      migrated = checkout.sameIndexers(this.linearizer.indexers)
+      closing = checkout.close()
+    }
+
     for (let i = 0; i < blocks.length; i++) {
       const { value, heads, batch } = localNodes[i]
 
       blocks[i] = {
         version: this.version,
         digest: this._addCheckpoints ? this._generateDigest() : null,
-        checkpoint: this._addCheckpoints ? await generateCheckpoint(cores) : null,
+        checkpoint: this._addCheckpoints ? await generateCheckpoint(cores, migrated) : null,
         node: {
           heads,
           batch,
@@ -1555,6 +1576,7 @@ module.exports = class Autobase extends ReadyResource {
     }
 
     await this.local.append(blocks)
+    await closing
 
     if (this._addCheckpoints) {
       const { checkpoint } = blocks[blocks.length - 1]
@@ -1565,11 +1587,11 @@ module.exports = class Autobase extends ReadyResource {
   }
 }
 
-function generateCheckpoint (cores) {
+function generateCheckpoint (cores, migrated) {
   const checkpoint = []
 
   for (const core of cores) {
-    checkpoint.push(core.checkpoint())
+    checkpoint.push(core.checkpoint(migrated))
     core.checkpointer++
   }
 
