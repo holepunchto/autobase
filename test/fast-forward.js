0e005c75a2dfea0c6a7b077a35d74cc83bcda3be
Christophe Diederichs <chm-diederichs@protonmail.com>
2024-02-22 11:01:22 +0000

    revert all

--- test/fast-forward.js
+++ test/fast-forward.js
@@ -14,7 +14,6 @@ const {
   createBase
 } = require('./helpers')
 
-for (let i = 0; i < 100; i++) {
   test('fast-forward - simple', async t => {
     t.plan(1)
 
@@ -108,276 +107,275 @@ for (let i = 0; i < 100; i++) {
     const core = c.view.getBackingCore()
     const sparse = await isSparse(core)
 
-    t.is(c.linearizer.indexers.length, 2)
+  t.is(c.linearizer.indexers.length, 2)
 
-    t.ok(sparse > 2000)
+  t.ok(sparse > 2000)
 
-    t.comment('sparse blocks: ' + sparse)
-    t.comment('percentage: ' + (sparse / core.length * 100).toFixed(2) + '%')
+  t.comment('sparse blocks: ' + sparse)
+  t.comment('percentage: ' + (sparse / core.length * 100).toFixed(2) + '%')
+})
+
+test('fast-forward - multiple writers added', async t => {
+  t.plan(2)
+
+  const MESSAGES_PER_ROUND = 200
+
+  const { bases } = await create(4, t, {
+    fastForward: true,
+    storage: () => tmpDir(t)
   })
 
-  test('fast-forward - multiple writers added', async t => {
-    t.plan(2)
+  const [a, b, c, d] = bases
 
-    const MESSAGES_PER_ROUND = 200
+  await addWriterAndSync(a, b)
+  await addWriterAndSync(a, c)
 
-    const { bases } = await create(4, t, {
-      fastForward: true,
-      storage: () => tmpDir(t)
-    })
+  await b.append('b')
+  await c.append('c')
 
-    const [a, b, c, d] = bases
+  await confirm([a, b, c])
 
-    await addWriterAndSync(a, b)
-    await addWriterAndSync(a, c)
+  const online = [a, b, c]
 
-    await b.append('b')
-    await c.append('c')
+  for (let i = 0; i < 10; i++) {
+    const unreplicate = replicate(online)
+    await eventFlush()
 
-    await confirm([a, b, c])
+    const as = Math.random() * MESSAGES_PER_ROUND
+    const bs = Math.random() * MESSAGES_PER_ROUND
+    const cs = Math.random() * MESSAGES_PER_ROUND
 
-    const online = [a, b, c]
+    for (let j = 0; j < Math.max(as, bs, cs); j++) {
+      if (j < as) a.append('a' + j)
+      if (j < bs) b.append('b' + j)
+      if (j < cs) c.append('c' + j)
 
-    for (let i = 0; i < 10; i++) {
-      const unreplicate = replicate(online)
-      await eventFlush()
-
-      const as = Math.random() * MESSAGES_PER_ROUND
-      const bs = Math.random() * MESSAGES_PER_ROUND
-      const cs = Math.random() * MESSAGES_PER_ROUND
-
-      for (let j = 0; j < Math.max(as, bs, cs); j++) {
-        if (j < as) a.append('a' + j)
-        if (j < bs) b.append('b' + j)
-        if (j < cs) c.append('c' + j)
-
-        if (j % 2 === 0) await eventFlush()
-      }
-
-      await unreplicate()
-      await confirm(online)
-
-      if (i === 8) online.push(d)
+      if (j % 2 === 0) await eventFlush()
     }
-
-    const core = d.view.getBackingCore()
-    const sparse = await isSparse(core)
-
-    t.is(d.linearizer.indexers.length, 3)
-
-    t.ok(sparse > 0)
-    t.comment('sparse blocks: ' + sparse)
-    t.comment('percentage: ' + (sparse / core.length * 100).toFixed(2) + '%')
-  })
-
-  test('fast-forward - multiple queues', async t => {
-    const { bases } = await create(4, t, {
-      fastForward: true,
-      storage: () => tmpDir(t)
-    })
-
-    const [a, b, c, d] = bases
-
-    // this value should be long enough that 2 fast-forwards are
-    // queued (ie. we don't just replicate the last state), but short
-    // enough that the second is queued before the first has completed
-    const DELAY = 20
-
-    await addWriterAndSync(a, b)
-    await addWriterAndSync(a, c, false)
-
-    await b.append('b')
-    await c.append('c')
-
-    await confirm([a, b, c])
-    await replicateAndSync([a, b, c, d])
-
-    for (let i = 0; i < 1000; i++) {
-      await a.append('a' + i)
-    }
-
-    await confirm([a, b, c])
-
-    const midLength = a.system.core.indexedLength
-
-    for (let i = 0; i < 1000; i++) {
-      await a.append('a' + i)
-    }
-
-    await confirm([a, b])
-
-    // trigger 1st fast-forward
-    const s1 = c.replicate(true)
-    const s2 = d.replicate(false)
-
-    s1.pipe(s2).pipe(s1)
-
-    await new Promise(resolve => s2.on('open', resolve))
-
-    // trigger 2nd fast-forward
-    setTimeout(() => t.teardown(replicate([a, b, d])), DELAY)
-
-    const to = await new Promise(resolve => d.on('fast-forward', resolve))
-    const next = new Promise(resolve => d.on('fast-forward', resolve))
-    let done = false
-
-    {
-      const pointer = await d.local.getUserData('autobase/system')
-      const { indexed } = cenc.decode(SystemPointer, pointer)
-      t.is(indexed.length, to)
-
-      done = to > midLength
-    }
-
-    if (done) {
-      // vary value of DELAY, but make sure the first fast-forward
-      // has not completed when the second is queued
-      // (check fastForwardTo !== null in queueFastForward)
-      t.fail('test failed due to timing')
-      return
-    }
-
-    const final = await next
-
-    {
-      const pointer = await d.local.getUserData('autobase/system')
-      const { indexed } = cenc.decode(SystemPointer, pointer)
-      t.is(indexed.length, final)
-    }
-  })
-
-  test('fast-forward - open with no remote io', async t => {
-    const { bases, stores } = await create(2, t, {
-      apply: applyOldState,
-      fastForward: true,
-      storage: () => tmpDir(t)
-    })
-
-    const [a, b] = bases
-
-    await b.ready()
-
-    for (let i = 0; i < 1000; i++) {
-      await a.append('a' + i)
-    }
-
-    await addWriterAndSync(a, b)
-    const unreplicate = replicate([a, b])
-
-    const core = b.view.getBackingCore()
-    const sparse = await isSparse(core)
-
-    t.ok(sparse > 0)
-    t.comment('sparse blocks: ' + sparse)
-
-    await b.append('b1')
-    await b.append('b2')
-    await b.append('b3')
 
     await unreplicate()
+    await confirm(online)
 
-    await a.append('a1001')
+    if (i === 8) online.push(d)
+  }
 
-    await b.close()
+  const core = d.view.getBackingCore()
+  const sparse = await isSparse(core)
 
-    const local = a.local
-    const remote = stores[1].get({ key: local.key })
+  t.is(d.linearizer.indexers.length, 3)
 
-    const s1 = local.replicate(true)
-    const s2 = remote.replicate(false)
+  t.ok(sparse > 0)
+  t.comment('sparse blocks: ' + sparse)
+  t.comment('percentage: ' + (sparse / core.length * 100).toFixed(2) + '%')
+})
 
-    s1.pipe(s2).pipe(s1)
+test('fast-forward - multiple queues', async t => {
+  const { bases } = await create(4, t, {
+    fastForward: true,
+    storage: () => tmpDir(t)
+  })
 
-    await remote.download({ end: local.length }).downloaded()
+  const [a, b, c, d] = bases
 
-    s1.destroy()
-    await new Promise(resolve => s2.on('close', resolve))
+  // this value should be long enough that 2 fast-forwards are
+  // queued (ie. we don't just replicate the last state), but short
+  // enough that the second is queued before the first has completed
+  const DELAY = 20
 
-    const b2 = await createBase(stores[1].session(), a.local.key, t, {
-      apply: applyOldState
-    })
+  await addWriterAndSync(a, b)
+  await addWriterAndSync(a, c, false)
 
-    await b2.ready()
-    await t.execution(b2.ready())
+  await b.append('b')
+  await c.append('c')
 
-    async function applyOldState (batch, view, base) {
-      for (const { value } of batch) {
-        if (value.add) {
-          const key = Buffer.from(value.add, 'hex')
-          await base.addWriter(key, { indexer: value.indexer })
-          continue
-        }
+  await confirm([a, b, c])
+  await replicateAndSync([a, b, c, d])
 
-        if (view) await view.append(value)
-        const core = view._source.core.session
+  for (let i = 0; i < 1000; i++) {
+    await a.append('a' + i)
+  }
 
-        // get well distributed unique index
-        const index = (view.length * 67 + view.length * 89) % core.length
-        if (core.length) await core.get(index)
+  await confirm([a, b, c])
+
+  const midLength = a.system.core.indexedLength
+
+  for (let i = 0; i < 1000; i++) {
+    await a.append('a' + i)
+  }
+
+  await confirm([a, b])
+
+  // trigger 1st fast-forward
+  const s1 = c.replicate(true)
+  const s2 = d.replicate(false)
+
+  s1.pipe(s2).pipe(s1)
+
+  await new Promise(resolve => s2.on('open', resolve))
+
+  // trigger 2nd fast-forward
+  setTimeout(() => t.teardown(replicate([a, b, d])), DELAY)
+
+  const to = await new Promise(resolve => d.on('fast-forward', resolve))
+  const next = new Promise(resolve => d.on('fast-forward', resolve))
+  let done = false
+
+  {
+    const pointer = await d.local.getUserData('autobase/system')
+    const { indexed } = cenc.decode(SystemPointer, pointer)
+    t.is(indexed.length, to)
+
+    done = to > midLength
+  }
+
+  if (done) {
+    // vary value of DELAY, but make sure the first fast-forward
+    // has not completed when the second is queued
+    // (check fastForwardTo !== null in queueFastForward)
+    t.fail('test failed due to timing')
+    return
+  }
+
+  const final = await next
+
+  {
+    const pointer = await d.local.getUserData('autobase/system')
+    const { indexed } = cenc.decode(SystemPointer, pointer)
+    t.is(indexed.length, final)
+  }
+})
+
+test('fast-forward - open with no remote io', async t => {
+  const { bases, stores } = await create(2, t, {
+    apply: applyOldState,
+    fastForward: true,
+    storage: () => tmpDir(t)
+  })
+
+  const [a, b] = bases
+
+  await b.ready()
+
+  for (let i = 0; i < 1000; i++) {
+    await a.append('a' + i)
+  }
+
+  await addWriterAndSync(a, b)
+  const unreplicate = replicate([a, b])
+
+  const core = b.view.getBackingCore()
+  const sparse = await isSparse(core)
+
+  t.ok(sparse > 0)
+  t.comment('sparse blocks: ' + sparse)
+
+  await b.append('b1')
+  await b.append('b2')
+  await b.append('b3')
+
+  await unreplicate()
+
+  await a.append('a1001')
+
+  await b.close()
+
+  const local = a.local
+  const remote = stores[1].get({ key: local.key })
+
+  const s1 = local.replicate(true)
+  const s2 = remote.replicate(false)
+
+  s1.pipe(s2).pipe(s1)
+
+  await remote.download({ end: local.length }).downloaded()
+
+  s1.destroy()
+  await new Promise(resolve => s2.on('close', resolve))
+
+  const b2 = await createBase(stores[1].session(), a.local.key, t, {
+    apply: applyOldState
+  })
+
+  await b2.ready()
+  await t.execution(b2.ready())
+
+  async function applyOldState (batch, view, base) {
+    for (const { value } of batch) {
+      if (value.add) {
+        const key = Buffer.from(value.add, 'hex')
+        await base.addWriter(key, { indexer: value.indexer })
+        continue
       }
+
+      if (view) await view.append(value)
+      const core = view._source.core.session
+
+      // get well distributed unique index
+      const index = (view.length * 67 + view.length * 89) % core.length
+      if (core.length) await core.get(index)
     }
+  }
+})
+
+test('fast-forward - force reset then ff', async t => {
+  t.plan(9)
+
+  const { bases } = await create(3, t, {
+    fastForward: true,
+    storage: () => tmpDir(t)
   })
 
-  test('fast-forward - force reset then ff', async t => {
-    t.plan(9)
+  const [a, b, c] = bases
 
-    const { bases } = await create(3, t, {
-      fastForward: true,
-      storage: () => tmpDir(t)
-    })
+  await addWriterAndSync(a, b)
+  await addWriterAndSync(a, c)
+  await confirm([a, b, c])
 
-    const [a, b, c] = bases
+  t.is(a.system.core.getBackingCore().manifest.signers.length, 3)
 
-    await addWriterAndSync(a, b)
-    await addWriterAndSync(a, c)
-    await confirm([a, b, c])
+  for (let i = 0; i < 2000; i++) {
+    await a.append('a' + i)
+  }
 
-    t.is(a.system.core.getBackingCore().manifest.signers.length, 3)
+  await replicateAndSync([a, b])
+  await b.append(null)
+  await replicateAndSync([a, b])
+  await a.append(null)
+  await replicateAndSync([a, b])
 
-    for (let i = 0; i < 2000; i++) {
-      await a.append('a' + i)
-    }
+  for (let i = 0; i < 2000; i++) {
+    await a.append('a' + i)
+  }
 
-    await replicateAndSync([a, b])
-    await b.append(null)
-    await replicateAndSync([a, b])
-    await a.append(null)
-    await replicateAndSync([a, b])
+  t.ok(b.system.core.getBackingCore().flushedLength > 2000)
+  t.ok(b.system.core.getBackingCore().indexedLength < 40)
 
-    for (let i = 0; i < 2000; i++) {
-      await a.append('a' + i)
-    }
+  await confirm([a, c])
 
-    t.ok(b.system.core.getBackingCore().flushedLength > 2000)
-    t.ok(b.system.core.getBackingCore().indexedLength < 40)
+  t.ok(a.system.core.getBackingCore().indexedLength > 4000)
 
-    await confirm([a, c])
+  const truncate = new Promise(resolve => b.system.core.on('truncate', resolve))
 
-    t.ok(a.system.core.getBackingCore().indexedLength > 4000)
+  t.not(b.system.core.getBackingCore().indexedLength, a.system.core.getBackingCore().indexedLength)
 
-    const truncate = new Promise(resolve => b.system.core.on('truncate', resolve))
+  await b.forceResetViews()
 
-    t.not(b.system.core.getBackingCore().indexedLength, a.system.core.getBackingCore().indexedLength)
+  await replicateAndSync([a, b, c])
 
-    await b.forceResetViews()
+  await t.execution(truncate)
 
-    await replicateAndSync([a, b, c])
+  t.is(b.system.core.getBackingCore().indexedLength, a.system.core.getBackingCore().indexedLength)
 
-    await t.execution(truncate)
+  await replicateAndSync([a, c])
 
-    t.is(b.system.core.getBackingCore().indexedLength, a.system.core.getBackingCore().indexedLength)
+  const core = b.system.core.getBackingCore()
+  const sparse = await isSparse(core)
 
-    await replicateAndSync([a, c])
+  t.is(c.linearizer.indexers.length, 3)
 
-    const core = b.system.core.getBackingCore()
-    const sparse = await isSparse(core)
-
-    t.is(c.linearizer.indexers.length, 3)
-
-    t.ok(sparse > 0)
-    t.comment('sparse blocks: ' + sparse)
-  })
-}
+  t.ok(sparse > 0)
+  t.comment('sparse blocks: ' + sparse)
+})
 
 async function isSparse (core) {
   let n = 0
