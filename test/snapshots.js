00697bbef87d10798a8cc46d1210d092ee97fef0
HDegroote <75906619+HDegroote@users.noreply.github.com>
2024-03-07 10:12:38 +0100

    Failing test where snapshot indexedLength changes

--- test/snapshots.js
+++ test/snapshots.js
@@ -110,7 +110,7 @@ test('no inconsistent snapshot-of-snapshot entries when truncated', async t => {
   t.alike(origValues2, newValues2)
 })
 
-test('no inconsistent entries when using snapshot core in bee (bee snapshot)', async t => {
+test.solo('no inconsistent entries when using snapshot core in bee (bee snapshot)', async t => {
   // Setup
   const { bases } = await create(3, t, {
     apply (...args) { return applyForBee(t, ...args) },
@@ -161,6 +161,11 @@ test('no inconsistent entries when using snapshot core in bee (bee snapshot)', a
   t.alike(keys2PreMerge, keys2PostMerge)
 
   t.is(hasTruncated, true) // Sanity check
+  t.is(bee1.core.indexedLength, 2, 'bee1 snapshot indexed length did not change')
+  t.is(bee1.version, 3, 'bee1 snapshot version did not change')
+
+  t.is(bee2.core.indexedLength, 2, 'bee2 snapshot indexed length did not change')
+  t.is(bee2.version, 4, 'bee2 snapshot version did not change')
 })
 
 test('check cloning detached snapshot', async t => {
