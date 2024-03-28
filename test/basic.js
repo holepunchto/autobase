b066f9ead0c8dc192a6dd9b2bda9dae65f309940
Christophe Diederichs <chm-diederichs@protonmail.com>
2024-03-28 17:27:18 +0000

    test checks core is properly loaded

--- test/basic.js
+++ test/basic.js
@@ -74,6 +74,8 @@ test('basic - local key pair', async t => {
 
   const base2 = await createBase(store, key, t)
   t.alike(base2.local.key, base.local.key)
+  t.alike(await base2.view.get(0), block)
+  t.is(base2.local.manifest.signers[0].publicKey, keyPair.publicKey)
 })
 
 test('basic - view', async t => {
