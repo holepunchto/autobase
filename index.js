cb8133a12145fea9112bb57b096a9404d86f3bfe
Christophe Diederichs <chm-diederichs@protonmail.com>
2024-04-04 11:02:41 +0100

    emit reindexing event when migrating v0 base

--- index.js
+++ index.js
@@ -90,6 +90,7 @@ module.exports = class Autobase extends ReadyResource {
 
     this._advancing = null
     this._advanced = null
+    this._reindexing = false
 
     this._bump = debounceify(() => {
       this._advancing = this._advance()
@@ -161,6 +162,10 @@ module.exports = class Autobase extends ReadyResource {
 
   get discoveryKey () {
     return this._primaryBootstrap === null ? this.local.discoveryKey : this._primaryBootstrap.discoveryKey
+  }
+
+  get reindexing () {
+    return this._indexing
   }
 
   replicate (init, opts) {
@@ -316,6 +321,13 @@ module.exports = class Autobase extends ReadyResource {
       await this._initialSystem.close()
       this._initialSystem = null
       this._initialViews = null
+    } else {
+      // check if this is a v0 base
+      const record = await this.getUserData('autobase/system')
+      if (record !== null) {
+        this.emit('reindex')
+        this._reindexing = true
+      }
     }
 
     // load previous digest if available
@@ -353,7 +365,7 @@ module.exports = class Autobase extends ReadyResource {
 
     // queue a full bump that handles wakeup etc (not legal to wait for that here)
     this._queueBump()
-    this._advanced = this._advancing
+    this._advanced = this._advancing.then(() => { this._reindexing = false }, safetyCatch)
 
     this.queueFastForward()
   }
