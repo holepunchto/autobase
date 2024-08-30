0da1e04975aa6fd416db5a9f14ef61504111f18c
Christophe Diederichs <chm-diederichs@protonmail.com>
2024-08-30 14:16:12 +0100

    add missing timeouts on hypercore gets

--- index.js
+++ index.js
@@ -1744,7 +1744,7 @@ module.exports = class Autobase extends ReadyResource {
 
       for (const { key, core, length } of indexers) {
         if (core.length === 0 && length > 0) promises.push(core.get(length - 1, { timeout }))
-        promises.push(system.get(key))
+        promises.push(system.get(key, { timeout }))
       }
 
       for (const { core, length } of pendingViews) {
