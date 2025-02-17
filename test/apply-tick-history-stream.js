a03c8ecb5ca5b7f64f0dda8262f634d0ff62e047
Sean Zellmer <sean@lejeunerenard.com>
2025-02-16 19:22:28 -0600

    Add `views` & `writer` to `system.history()`'s `updates`
    Merges the previous `system.createApplyTickHistoryStream()` with
    `system.history()` as they are virtually the same.

--- test/apply-tick-history-stream.js
+++ test/apply-tick-history-stream.js
@@ -7,23 +7,9 @@ const {
   addWriterAndSync
 } = require('./helpers')
 
-function collect (stream) {
-  return new Promise((resolve, reject) => {
-    const entries = []
-    let ended = false
-    stream.on('data', d => entries.push(d))
-    stream.on('error', err => reject(err))
-    stream.on('end', () => { ended = true })
-    stream.on('close', () => {
-      if (ended) resolve(entries)
-      else reject(new Error('Premature close'))
-    })
-  })
-}
-
 const totalAppendsFromHistory = (h) => {
   let totalAppends = 0
-  for (const update of h) {
+  for (const update of h.updates) {
     let totalViewAppends = 0
     for (const view of update.views) {
       totalViewAppends += view.appends
@@ -57,16 +43,16 @@ test('apply history - simple 2', async t => {
 
   await replicateAndSync(bases)
 
-  const h = await collect(a.system.createApplyTickHistoryStream())
+  const h = await a.system.history(0)
 
-  t.is(h.length, 6)
-  t.alike(h, [
-    { index: 4, views: [], writer: { key: a.local.key, length: 1 } }, // adding b as indexer
-    { index: 6, views: [], writer: { key: b.local.key, length: 1 } }, // b no longer pending via ack
-    { index: 8, views: [], writer: { key: a.local.key, length: 2 } }, // a acking b for consensus
-    { index: 10, views: [{ key: a.view.key, appends: 1 }], writer: { key: a.local.key, length: 3 } }, // a0
-    { index: 12, views: [{ key: a.view.key, appends: 1 }], writer: { key: b.local.key, length: 2 } }, // b0
-    { index: 14, views: [{ key: a.view.key, appends: 1 }], writer: { key: a.local.key, length: 4 } } // a1
+  t.is(h.updates.length, 6)
+  t.alike(h.updates, [
+    { batch: 1, indexers: true, systemLength: 5, views: [], writer: { key: a.local.key, length: 1 } }, // adding b as indexer
+    { batch: 1, indexers: true, systemLength: 7, views: [], writer: { key: b.local.key, length: 1 } }, // b no longer pending via ack
+    { batch: 1, indexers: false, systemLength: 9, views: [], writer: { key: a.local.key, length: 2 } }, // a acking b for consensus
+    { batch: 1, indexers: false, systemLength: 11, views: [{ key: a.view.key, appends: 1 }], writer: { key: a.local.key, length: 3 } }, // a0
+    { batch: 1, indexers: false, systemLength: 13, views: [{ key: a.view.key, appends: 1 }], writer: { key: b.local.key, length: 2 } }, // b0
+    { batch: 1, indexers: false, systemLength: 15, views: [{ key: a.view.key, appends: 1 }], writer: { key: a.local.key, length: 4 } } // a1
   ])
   t.is(totalAppendsFromHistory(h), a.view.length)
 })
@@ -94,16 +80,16 @@ test('apply history - batch writer append', async t => {
 
   await replicateAndSync(bases)
 
-  const h = await collect(a.system.createApplyTickHistoryStream())
+  const h = await a.system.history(0)
 
-  t.is(h.length, 6)
-  t.alike(h, [
-    { index: 4, views: [], writer: { key: a.local.key, length: 1 } },
-    { index: 6, views: [], writer: { key: b.local.key, length: 1 } }, // b no longer pending via ack
-    { index: 8, views: [], writer: { key: a.local.key, length: 2 } }, // a acking b for consensus
-    { index: 10, views: [{ key: a.view.key, appends: 3 }], writer: { key: a.local.key, length: 5 } },
-    { index: 12, views: [{ key: a.view.key, appends: 4 }], writer: { key: b.local.key, length: 5 } },
-    { index: 14, views: [{ key: a.view.key, appends: 1 }], writer: { key: a.local.key, length: 6 } }
+  t.is(h.updates.length, 6)
+  t.alike(h.updates, [
+    { batch: 1, indexers: true, systemLength: 5, views: [], writer: { key: a.local.key, length: 1 } },
+    { batch: 1, indexers: true, systemLength: 7, views: [], writer: { key: b.local.key, length: 1 } }, // b no longer pending via ack
+    { batch: 1, indexers: false, systemLength: 9, views: [], writer: { key: a.local.key, length: 2 } }, // a acking b for consensus
+    { batch: 3, indexers: false, systemLength: 11, views: [{ key: a.view.key, appends: 3 }], writer: { key: a.local.key, length: 5 } },
+    { batch: 4, indexers: false, systemLength: 13, views: [{ key: a.view.key, appends: 4 }], writer: { key: b.local.key, length: 5 } },
+    { batch: 1, indexers: false, systemLength: 15, views: [{ key: a.view.key, appends: 1 }], writer: { key: a.local.key, length: 6 } }
   ])
   t.is(totalAppendsFromHistory(h), a.view.length)
 })
@@ -133,16 +119,16 @@ test('apply history - add writer w/ existing view', async t => {
 
   await replicateAndSync(bases)
 
-  const h = await collect(a.system.createApplyTickHistoryStream())
+  const h = await a.system.history(0)
 
-  t.is(h.length, 6)
-  t.alike(h, [
-    { index: 3, views: [{ key: viewV0Key, appends: 3 }], writer: { key: a.local.key, length: 3 } },
-    { index: 6, views: [], writer: { key: a.local.key, length: 4 } },
-    { index: 8, views: [], writer: { key: b.local.key, length: 1 } }, // b no longer pending via ack
-    { index: 10, views: [], writer: { key: a.local.key, length: 5 } }, // a acking b for consensus
-    { index: 12, views: [{ key: viewV1Key, appends: 4 }], writer: { key: b.local.key, length: 5 } },
-    { index: 14, views: [{ key: viewV1Key, appends: 1 }], writer: { key: a.local.key, length: 6 } }
+  t.is(h.updates.length, 6)
+  t.alike(h.updates, [
+    { batch: 3, indexers: true, systemLength: 4, views: [{ key: viewV0Key, appends: 3 }], writer: { key: a.local.key, length: 3 } },
+    { batch: 1, indexers: false, systemLength: 7, views: [], writer: { key: a.local.key, length: 4 } },
+    { batch: 1, indexers: true, systemLength: 9, views: [], writer: { key: b.local.key, length: 1 } }, // b no longer pending via ack
+    { batch: 1, indexers: false, systemLength: 11, views: [], writer: { key: a.local.key, length: 5 } }, // a acking b for consensus
+    { batch: 4, indexers: false, systemLength: 13, views: [{ key: viewV1Key, appends: 4 }], writer: { key: b.local.key, length: 5 } },
+    { batch: 1, indexers: false, systemLength: 15, views: [{ key: viewV1Key, appends: 1 }], writer: { key: a.local.key, length: 6 } }
   ])
   t.is(totalAppendsFromHistory(h), a.view.length)
 })
@@ -174,12 +160,14 @@ test('apply history - add writer w/ multiple views', async t => {
 
   await replicateAndSync(bases)
 
-  const h = await collect(a.system.createApplyTickHistoryStream())
+  const h = await a.system.history(0)
 
-  t.is(h.length, 6)
-  t.alike(h, [
+  t.is(h.updates.length, 6)
+  t.alike(h.updates, [
     {
-      index: 3,
+      batch: 3,
+      indexers: true,
+      systemLength: 4,
       views: [
         {
           key: view0V0Key,
@@ -192,11 +180,13 @@ test('apply history - add writer w/ multiple views', async t => {
       ],
       writer: { key: a.local.key, length: 3 }
     },
-    { index: 6, views: [], writer: { key: a.local.key, length: 4 } },
-    { index: 8, views: [], writer: { key: b.local.key, length: 1 } }, // b no longer pending via ack
-    { index: 10, views: [], writer: { key: a.local.key, length: 5 } }, // a acking b for consensus
+    { batch: 1, indexers: false, systemLength: 7, views: [], writer: { key: a.local.key, length: 4 } },
+    { batch: 1, indexers: true, systemLength: 9, views: [], writer: { key: b.local.key, length: 1 } }, // b no longer pending via ack
+    { batch: 1, indexers: false, systemLength: 11, views: [], writer: { key: a.local.key, length: 5 } }, // a acking b for consensus
     {
-      index: 12,
+      batch: 4,
+      indexers: false,
+      systemLength: 13,
       views: [
         {
           key: view0V1Key,
@@ -210,7 +200,9 @@ test('apply history - add writer w/ multiple views', async t => {
       writer: { key: b.local.key, length: 5 }
     },
     {
-      index: 14,
+      batch: 1,
+      indexers: false,
+      systemLength: 15,
       views: [
         {
           key: view0V1Key,
@@ -274,17 +266,17 @@ test('apply history - writer removed', async t => {
   await replicateAndSync(bases)
   await replicateAndSync(bases)
 
-  const h = await collect(a.system.createApplyTickHistoryStream())
+  const h = await a.system.history(0)
 
-  t.is(h.length, 7)
-  t.alike(h, [
-    { index: 4, views: [], writer: { key: a.local.key, length: 1 } },
-    { index: 6, views: [], writer: { key: b.local.key, length: 1 } }, // b no longer pending via ack
-    { index: 8, views: [], writer: { key: a.local.key, length: 2 } }, // a acking b for consensus
-    { index: 10, views: [{ key: a.view.key, appends: 3 }], writer: { key: a.local.key, length: 5 } },
-    { index: 12, views: [{ key: a.view.key, appends: 4 }], writer: { key: b.local.key, length: 5 } },
-    { index: 14, views: [{ key: a.view.key, appends: 1 }], writer: { key: a.local.key, length: 6 } },
-    { index: 17, views: [], writer: { key: a.local.key, length: 7 } } // Removal
+  t.is(h.updates.length, 7)
+  t.alike(h.updates, [
+    { batch: 1, indexers: true, systemLength: 5, views: [], writer: { key: a.local.key, length: 1 } },
+    { batch: 1, indexers: true, systemLength: 7, views: [], writer: { key: b.local.key, length: 1 } }, // b no longer pending via ack
+    { batch: 1, indexers: false, systemLength: 9, views: [], writer: { key: a.local.key, length: 2 } }, // a acking b for consensus
+    { batch: 3, indexers: false, systemLength: 11, views: [{ key: a.view.key, appends: 3 }], writer: { key: a.local.key, length: 5 } },
+    { batch: 4, indexers: false, systemLength: 13, views: [{ key: a.view.key, appends: 4 }], writer: { key: b.local.key, length: 5 } },
+    { batch: 1, indexers: false, systemLength: 15, views: [{ key: a.view.key, appends: 1 }], writer: { key: a.local.key, length: 6 } },
+    { batch: 1, indexers: true, systemLength: 18, views: [], writer: { key: a.local.key, length: 7 } } // Removal
   ])
   t.is(totalAppendsFromHistory(h), a.view.length)
 
@@ -303,39 +295,4 @@ test('apply history - writer removed', async t => {
       await view.append(value)
     }
   }
-})
-
-test('apply history - ignores reverse option', async t => {
-  const { bases } = await create(2, t)
-
-  const [a, b] = bases
-
-  let ai = 0
-  let bi = 0
-
-  await addWriterAndSync(a, b)
-
-  await a.append('a' + ai++)
-  await replicateAndSync(bases)
-
-  await b.append('b' + bi++)
-  await replicateAndSync(bases)
-
-  await a.append('a' + ai++)
-  await replicateAndSync(bases)
-
-  await replicateAndSync(bases)
-
-  const h = await collect(a.system.createApplyTickHistoryStream({ reverse: true }))
-
-  t.is(h.length, 6)
-  t.alike(h, [
-    { index: 4, views: [], writer: { key: a.local.key, length: 1 } }, // adding b as indexer
-    { index: 6, views: [], writer: { key: b.local.key, length: 1 } }, // b no longer pending via ack
-    { index: 8, views: [], writer: { key: a.local.key, length: 2 } }, // a acking b for consensus
-    { index: 10, views: [{ key: a.view.key, appends: 1 }], writer: { key: a.local.key, length: 3 } }, // a0
-    { index: 12, views: [{ key: a.view.key, appends: 1 }], writer: { key: b.local.key, length: 2 } }, // b0
-    { index: 14, views: [{ key: a.view.key, appends: 1 }], writer: { key: a.local.key, length: 4 } } // a1
-  ])
-  t.is(totalAppendsFromHistory(h), a.view.length)
 })
