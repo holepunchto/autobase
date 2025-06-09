a8c715184324bb0b94089a8d55fa6c00a40b2df1
Christophe Diederichs <chm-diederichs@protonmail.com>
2025-06-09 16:40:55 +0100

    increase default fuzz parameters

--- test/fuzz/index.js
+++ test/fuzz/index.js
@@ -1,11 +1,11 @@
 const path = require('path')
 const { fuzz } = require('./helpers')
 
-const writers = 10
-const indexers = 3
+const writers = 30
+const indexers = 5
 const nodes = 20
 const branch = 0.4
-const optimistic = 0.1 // disabled
+const optimistic = 0.1
 const log = 1000
 const dir = path.join(__dirname, process.argv[2] || 'generated')
 
