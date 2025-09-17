1f23901bbe92818b8d01016e220319f474ef0169
Sean Zellmer <sean@lejeunerenard.com>
2025-09-17 15:05:39 -0500

    Revert places where the issue doesnt apply
    When the promise is allocated during the same tick as it is awaited, it
    will be caught.

--- test/fixtures/generate/encryption.js
+++ test/fixtures/generate/encryption.js
@@ -42,15 +42,10 @@ async function main () {
 
   for (const data of DATA) await base.append(data)
 
-  const [local, system, view] = await Promise.all([
-    getBlocks(base.local),
-    getBlocks(base.core),
-    getBlocks(base.view)
-  ])
   const fixture = {
-    local,
-    system,
-    view
+    local: await getBlocks(base.local),
+    system: await getBlocks(base.core),
+    view: await getBlocks(base.view)
   }
 
   await fs.writeFile(fixturePath, JSON.stringify(fixture))
